dbf5645f9f502b27aab6ef94ecc324f94f294fff
Jenn L <jenn.lazariuk@gmail.com>
2015-06-25 19:11:01 -1000

    finished sort and splice

--- arrayMethods.js
+++ arrayMethods.js
@@ -46,8 +46,21 @@ mixedWords.sort();
 // SPLICE //
 
 var notFruit = fruitCollection.splice(1,3);
+
 gemBox.splice(5,2);
 gemBox.splice(6,4);
+
+upToTen.splice(2,0,3,4,5,6,7,8);
+
+brownOnly.splice(1,5,"brown", "brown", "brown");
+
+// UNSHIFT //
+
+orderedValues.unshift();
+
+
+
+
 
 
 
